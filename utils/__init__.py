cad56a6621a2802344a1ab46c7146a8a670dc1d0
bonokat <voevodina.kathrine@gmail.com>
2025-11-28 15:53:47 +1030

    S2 - erpac sensors + conditions draft

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,3 +5,27 @@ def check_paths(*paths: str) -> None:
         if not os.path.exists(path):
             os.mkdir(path)
 
+
+def iterate_conditions(groups, tasks, task_stages, subjects=None):
+    """
+    Yield all valid combinations of (group, task, task_stage, block, subject).
+
+    If subjects is None, yield 4-tuples:
+        (group, task, task_stage, block)
+
+    If subjects is provided, yield 5-tuples:
+        (group, task, task_stage, block, subject)
+    """
+    for group in groups:
+        for task in tasks:
+            # determine blocks
+            blocks = [''] if task == '_BL' else ['_baseline', '_adaptation']
+
+            for task_stage in task_stages:
+                for block in blocks:
+                    if subjects is None:
+                        # no subject-level iteration
+                        yield group, task, task_stage, block
+                    else:
+                        for subject in subjects:
+                            yield group, task, task_stage, block, subject
